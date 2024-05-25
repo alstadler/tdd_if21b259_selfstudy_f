d9a31e5650472e4b146c9d30b6ba14cda9e55816
alstadler00 <alstadler.email@gmail.com>
2024-05-25 03:07:06 +0200

    added unit test 2

--- spec/kata2_spec.js
+++ spec/kata2_spec.js
@@ -2,4 +2,8 @@ describe ('electrons_around_cores', function () {
     it('should calculate correct output for [1, 2, 3, 4, 5]', () => {
     expect(electronsAroundCores([1, 2, 3, 4, 5])).toBe(6);
     });
+
+    it('should calculate correct output for [2, 2, 3, 3]', () => {
+        expect(electronsAroundCores([2, 2, 3, 3])).toBe(4);
+    });
 })
